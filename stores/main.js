24188894915ddec340eb06e8b2da6d223397bac5
gironjhonatan <gironjhonatan85@gmail.com>
2022-05-12 14:26:47 -0500

    creacion de js con variables

--- stores/main.js
+++ stores/main.js
@@ -1,1 +1,7 @@
+const doc = document, 
+$tabla = doc.querySelector(".c-tabla"),
+$formulario = doc.querySelector(".c-formulario"),
+$titulo = doc.querySelector("c-titulo"),
+$template = doc.getElementById("c-template").content,
+$fragment = doc.createDocumentFragment();
 
